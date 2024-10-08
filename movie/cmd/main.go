68319cfd5a2d81fff54f729cdd84c8bf6db1d06f
ataha1 <a.taha12322@gmail.com>
2024-10-08 12:25:57 +0300

    add consul-based service discovery configurations to services

--- movie/cmd/main.go
+++ movie/cmd/main.go
@@ -1,23 +1,52 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/ataha1/movie-app/movie/internal/controller/movie"
 	metadataGateway "github.com/ataha1/movie-app/movie/internal/gateway/metadata/http"
 	ratingGateWay "github.com/ataha1/movie-app/movie/internal/gateway/rating/http"
 	httpHandler "github.com/ataha1/movie-app/movie/internal/handler/http"
+	"github.com/ataha1/movie-app/pkg/discovery"
+	"github.com/ataha1/movie-app/pkg/discovery/consul"
 )
 
+const serviceName = "movies"
+
 func main() {
-	log.Print("Starting the movie service")
-	metadataGateway := metadataGateway.New("localhost:8081")
-	ratingGateWay := ratingGateWay.New("localhost:8082")
-	ctrl := movie.New(metadataGateway, ratingGateWay)
-	h := httpHandler.New(ctrl)
+	var port int
+	flag.IntVar(&port, "port", 8083, "API handler port")
+	flag.Parse()
+	log.Printf("Starting the movie service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil{
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil{
+		panic(err)
+	}
+	go func ()  {
+		for{
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil{
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
+	metadataGateway := metadataGateway.New(registry)
+	ratingGateWay := ratingGateWay.New(registry)
+	svc := movie.New(metadataGateway, ratingGateWay)
+	h := httpHandler.New(svc)
 	http.Handle("/movie", http.HandlerFunc(h.GetMovieDetails))
-	if err := http.ListenAndServe(":8083", nil); err != nil{
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil{
 		panic(err)
 	}
 }
--- rating/cmd/main.go
+++ rating/cmd/main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/ataha1/movie-app/pkg/discovery"
+	"github.com/ataha1/movie-app/pkg/discovery/consul"
 	"github.com/ataha1/movie-app/rating/internal/controller/rating"
 	httphandler "github.com/ataha1/movie-app/rating/internal/handler/http"
 	"github.com/ataha1/movie-app/rating/internal/repository/memory"
 )
 
+const serviceName = "rating"
+
 func main() {
-	log.Println("Starting rating serviece")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Printf("Starting rating service on port %d", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil{
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil{
+		panic(err)
+	}
+	go func ()  {
+		for{
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil{
+				log.Println("Failed to report healthy state: " + err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer registry.Deregister(ctx, instanceID, serviceName)
 	repo := memory.New()
-	ctrl := rating.New(repo)
-	h := httphandler.New(ctrl)
-	http.Handle("GET", http.HandlerFunc(h.Handle))
-	if err := http.ListenAndServe(":8082", nil); err != nil{
+	svc := rating.New(repo)
+	h := httphandler.New(svc)
+	http.Handle("/rating", http.HandlerFunc(h.Handle))
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil{
 		panic(err)
 	}
 }
